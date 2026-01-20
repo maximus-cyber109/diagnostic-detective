5264772388f622ea9c632da22c9b9d19654ccfe4
maximus-cyber109 <scraper.updates.pinkblue@gmail.com>
2026-01-20 20:48:24 +0530

    Update get-config.js

--- netlify/functions/get-config.js
+++ netlify/functions/get-config.js
@@ -1,14 +1,16 @@
-// functions/get-config.js - Provide public Supabase config to frontend
+// functions/get-config.js
 exports.handler = async (event, context) => {
-    return {
-        statusCode: 200,
-        headers: {
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify({
-            supabaseUrl: process.env.SUPABASE_URL,
-            supabaseKey: process.env.SUPABASE_ANON_KEY // Only public anon key
-        })
-    };
+  return {
+    statusCode: 200,
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type'
+    },
+    body: JSON.stringify({
+      supabaseUrl: process.env.SUPABASE_URL || '',
+      supabaseKey: process.env.SUPABASE_ANON_KEY || ''
+    })
+  };
 };
